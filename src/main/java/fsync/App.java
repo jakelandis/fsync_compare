220c696784ee4ddab7cd71ce319f55cc9e5e7260
Jake Landis <jake.landis@elastic.co>
2017-06-19 17:01:42 -0500

    update with optional argument channel/mmap

--- src/main/java/fsync/App.java
+++ src/main/java/fsync/App.java
@@ -7,7 +7,14 @@ public class App {
     public final static long ONE_GB = 1024 * 1024 * 1024;
 
     public static void main(String... args) throws IOException {
-        Channel.doIt();
-        Mmap.doIt();
+        if ("channel".equalsIgnoreCase(args[0])) {
+            Channel.doIt();
+        } else if ("mmap".equalsIgnoreCase(args[0])) {
+            Mmap.doIt();
+        } else {
+            Channel.doIt();
+            Mmap.doIt();
+        }
+
     }
 }
