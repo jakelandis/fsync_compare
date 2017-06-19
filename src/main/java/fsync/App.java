b7ba524029407df661d06be09dc63457b6e8497e
Jordan Sissel <jls@semicomplete.com>
2017-06-19 15:37:13 -0700

    Refactor to have channel/mmap focus on those operations specifically and move test operations to App

--- src/main/java/fsync/App.java
+++ src/main/java/fsync/App.java
@@ -1,13 +1,55 @@
 package fsync;
 
 import java.io.IOException;
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class App {
+  public interface IO {
+    void write(ByteBuffer buffer) throws IOException;
+    void sync() throws IOException;
+  }
 
-    public final static long ONE_GB = 1024 * 1024 * 1024;
+  static final byte[] logEntry = "[Fri Sep 09 10:42:29.902022 2011] [core:error] [pid 35708:tid 4328636416] [client 72.15.99.187] File does not exist: /usr/local/apache2/htdocs/favicon.ico".getBytes(StandardCharsets.UTF_8);
+  public final static long ONE_GB = 1024 * 1024 * 1024;
+  public final static int SYNC_INTERVAL = 1024;
 
-    public static void main(String... args) throws IOException {
-        Channel.doIt();
-        Mmap.doIt();
+  public static void main(String... args) throws IOException {
+    final IO io = getIO(args[0]);
+    System.out.printf("Running %s test\n", args[0]);
+
+    final ByteBuffer buffer = ByteBuffer.wrap(logEntry);
+    buffer.rewind();
+
+    long bytes = 0;
+    int count = 0;
+    while (bytes < ONE_GB) {
+      io.write(buffer);
+      buffer.rewind();
+      bytes += logEntry.length;
+
+      if (count == SYNC_INTERVAL) {
+        io.sync();
+        count = 0;
+      }
     }
+
+    System.out.printf("Wrote %d bytes total\n", bytes);
+  }
+
+  public static IO getIO(String name) throws IOException {
+    File file = File.createTempFile(name, ".tmp");
+    file.deleteOnExit();
+
+    switch (name) {
+      case "channel": 
+        return new Channel(file);
+      case "mmap":
+        return  new Mmap(file, ONE_GB);
+      default:
+        System.out.println("Invalid test name");
+        return null;
+    }
+  }
 }
--- src/main/java/fsync/Mmap.java
+++ src/main/java/fsync/Mmap.java
@@ -6,62 +6,23 @@ import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.charset.StandardCharsets;
 
-public class Mmap {
-    private static final byte[] logEntry = "[Fri Sep 09 10:42:29.902022 2011] [core:error] [pid 35708:tid 4328636416] [client 72.15.99.187] File does not exist: /usr/local/apache2/htdocs/favicon.ico".getBytes(StandardCharsets.UTF_8);
+public class Mmap implements App.IO {
+  private MappedByteBuffer map;
 
-    static void doIt() throws IOException {
+  public Mmap(File file, long size) throws IOException { 
+    RandomAccessFile raf = new RandomAccessFile(file, "rw");
+    raf.setLength(size);
+    FileChannel fileChannel = raf.getChannel();
+    map = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, (int)size);
+    raf.close();
+  }
 
-        System.out.println("MMap Test");
-        System.out.println("==============");
+  public void write(ByteBuffer buffer) throws IOException {
+    map.put(buffer);
+  }
 
-        File file = File.createTempFile("mmap", ".tmp");
-        System.out.println("Creating a 1GB tempfile : " + file.getAbsolutePath());
-        file.deleteOnExit();
-        RandomAccessFile raf = new RandomAccessFile(file, "rw");
-
-        FileChannel fileChannel = raf.getChannel();
-
-        MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, App.ONE_GB);
-        raf.close();
-        mappedByteBuffer.load();
-
-        ByteBuffer preByteBuffer = ByteBuffer.allocateDirect(logEntry.length);
-        preByteBuffer.wrap(logEntry);
-
-        long allTime = 0;
-        long fsycTotalTime = 0;
-
-        int i = 0;
-        long bytesWritten = 0;
-        while (bytesWritten < App.ONE_GB - bytesWritten) {
-            long astart = System.nanoTime();
-            mappedByteBuffer.put(preByteBuffer);
-            preByteBuffer.rewind();
-
-            //fsync every 1024 writes
-            if (++i % 1024 == 0) {
-                if (i % 102400 == 0) {
-                    System.out.println(".");
-                } else {
-                    System.out.print(".");
-                }
-
-                long fstart = System.nanoTime();
-                mappedByteBuffer.force();
-                long fstop = System.nanoTime();
-                fsycTotalTime += fstop - fstart;
-            }
-            long astop = System.nanoTime();
-            bytesWritten += logEntry.length;
-            allTime += astop - astart;
-        }
-        System.out.println("");
-        System.out.println("# of Writes : " + i);
-        System.out.printf("%-26s : %s %s%n", "All total time", allTime, "ns");
-        System.out.printf("%-26s : %s %s%n", "Fsync total time", fsycTotalTime, "ns");
-        System.out.printf("%-26s : %s %s%n", "Fsync Percent of total time", Long.valueOf(fsycTotalTime).doubleValue() / Long.valueOf(allTime).doubleValue() * 100, "%");
-    }
-
+  public void sync() throws IOException {
+    map.force();
+  }
 }
